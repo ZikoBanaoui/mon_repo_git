e46ac0fc0bc9113fdb94a78a021090a325908935
Zakaria Banaoui <ziko.banaoui@gmail.com>
2019-03-08 21:07:18 +0100

    Définir le prototype Film avec ses deux méthodes init() et decrire()

--- codeScript2.js
+++ codeScript2.js
@@ -1,1 +1,10 @@
-Commande ECHO activ‚e.
+var Film={
+    init: function(titre,annee){
+        this.titre=titre;
+        this.annee=annee;
+    },
+    decrire: function(){
+        var description=this.titre+"( "+this.annee+") .";
+        return description;
+    }
+};
