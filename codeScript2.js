d8d6fc29919173d59f8094c52ed8b048be2fafc7
Zakaria Banaoui <ziko.banaoui@gmail.com>
2019-03-08 21:09:01 +0100

    Création de trois objets film1,film2,film3 et l'affichage

--- codeScript2.js
+++ codeScript2.js
@@ -8,3 +8,21 @@ var Film={
         return description;
     }
 };
+var film1=Object.create(Film);
+film1.init("Malcom X",1992);
+
+var film2=Object.create(Film);
+film2.init("Batman",2008);
+
+var film3=Object.create(Film);
+film3.init("spiderman",2018);
+
+var films=[];
+films.push(film1);
+films.push(film2);
+films.push(film3);
+
+films.forEach(function(film){
+    console.log(film.decrire());
+    
+});
